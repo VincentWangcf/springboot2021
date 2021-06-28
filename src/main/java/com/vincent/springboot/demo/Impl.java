21c8e7887cac47dc4326694d2fd73c27e35f8ee9
vincent <445047477@qq.com>
2021-06-28 22:30:54 +0800

    工厂模式

---
+++ src/main/java/com/vincent/springboot/demo/Impl.java
@@ -0,0 +1,10 @@
+package com.vincent.springboot.demo;
+
+public class Impl implements Api {
+    public void test1(String s) {
+        System.out.println("Now In Impl. The input s==" + s);
+    }
+}
+
+
+
--- src/test/java/com/vincent/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/vincent/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,7 @@
 package com.vincent.springboot;
 
+import com.vincent.springboot.demo.Api;
+import com.vincent.springboot.demo.Impl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class SpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Api api = new Impl();
+		api.test1("哈哈，不要紧张，只是个测试而已！");
 	}
 
 }
